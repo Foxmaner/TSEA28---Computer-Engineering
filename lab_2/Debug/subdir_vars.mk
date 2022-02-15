cc50ffdbb8652b806411ad5b0cff2fe7c327e845
eskbr129 <eskbr129@muxen2-012.ad.liu.se>
2022-02-15 16:18:42 +0100

    Empty lab2

---
+++ lab_2/Debug/subdir_vars.mk
@@ -0,0 +1,41 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../tm4c123gh6pm.cmd 
+
+ASM_SRCS += \
+../lab2.asm 
+
+C_SRCS += \
+../tm4c123gh6pm_startup_ccs.c 
+
+C_DEPS += \
+./tm4c123gh6pm_startup_ccs.d 
+
+OBJS += \
+./lab2.obj \
+./tm4c123gh6pm_startup_ccs.obj 
+
+ASM_DEPS += \
+./lab2.d 
+
+OBJS__QUOTED += \
+"lab2.obj" \
+"tm4c123gh6pm_startup_ccs.obj" 
+
+C_DEPS__QUOTED += \
+"tm4c123gh6pm_startup_ccs.d" 
+
+ASM_DEPS__QUOTED += \
+"lab2.d" 
+
+ASM_SRCS__QUOTED += \
+"../lab2.asm" 
+
+C_SRCS__QUOTED += \
+"../tm4c123gh6pm_startup_ccs.c" 
+
+
